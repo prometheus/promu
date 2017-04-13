1f90a9391ae824a0b9ee67b646b377ab48506c23
Tobias Schmidt <tobidt@gmail.com>
2017-04-13 13:11:52 -0300

    Use go1.8.1 by default

--- cmd/crossbuild.go
+++ cmd/crossbuild.go
@@ -74,7 +74,7 @@ func init() {
 	viper.BindPFlag("go.version", crossbuildCmd.Flags().Lookup("go"))
 
 	// Current bug in viper: SeDefault doesn't work with nested key
-	// viper.SetDefault("go.version", "1.8")
+	// viper.SetDefault("go.version", "1.8.1")
 	// platforms := defaultMainPlatforms
 	// platforms = append(platforms, defaultARMPlatforms...)
 	// platforms = append(platforms, defaultPowerPCPlatforms...)
