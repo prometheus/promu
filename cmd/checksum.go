dd97dceac4159e222707ff45a02d3fae13b4d0ac
Tobias Schmidt <tobidt@gmail.com>
2017-04-14 23:11:55 -0300

    Add promu checksum command
    This is essentially a reimplementation of sha256sum, but removing the
    directory name, thus removing the need to use more involved cd/grep
    usage in circle.yml files.
    
    I'm not sure if it's worth to reimplement such functionality, but it
    reduces the amount of shell scripts and it seems that what we're aiming
    for with promu?

---
+++ cmd/checksum.go
@@ -0,0 +1,106 @@
+// Copyright © 2017 Prometheus Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	checksumsFilename = "sha256sums.txt"
+)
+
+// checksumCmd represents the command calculating checksums for all files in a
+// given directory and writing the result to a checksums file.
+var checksumCmd = &cobra.Command{
+	Use:   "checksum [<location>]",
+	Short: "Calculate the SHA256 checksum for each file in the given location",
+	Long:  `Calculate the SHA256 checksum for each file in the given location`,
+	Run: func(cmd *cobra.Command, args []string) {
+		runChecksum(optArg(args, 0, "."))
+	},
+}
+
+// init prepares cobra flags
+func init() {
+	Promu.AddCommand(checksumCmd)
+}
+
+func runChecksum(path string) {
+	checksums, err := calculateSHA256s(path)
+	if err != nil {
+		fatalMsg("Failed to calculate checksums", err)
+	}
+
+	file, err := os.Create(filepath.Join(path, checksumsFilename))
+	if err != nil {
+		fatalMsg("Failed to create checksums file", err)
+	}
+	defer file.Close()
+	for _, c := range checksums {
+		if _, err := fmt.Fprintf(file, "%x  %s\n", c.checksum, c.filename); err != nil {
+			fatalMsg("Failed to write to checksums file", err)
+		}
+	}
+}
+
+type checksumSHA256 struct {
+	filename string
+	checksum []byte
+}
+
+// calculateSHA256s calculates the sha256 checksum for each file in the given
+// path and returns a checksumSHA256 type in the order returned of
+// filepath.Walk.
+func calculateSHA256s(path string) ([]checksumSHA256, error) {
+	var checksums []checksumSHA256
+	path = fmt.Sprintf("%s%c", filepath.Clean(path), filepath.Separator)
+	calculateSHA256 := func(filepath string, f os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if f.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(filepath)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		hash := sha256.New()
+		if _, err = io.Copy(hash, file); err != nil {
+			return err
+		}
+		checksums = append(checksums, checksumSHA256{
+			filename: strings.TrimPrefix(filepath, path),
+			checksum: hash.Sum(nil),
+		})
+
+		return nil
+	}
+	if err := filepath.Walk(path, calculateSHA256); err != nil {
+		return nil, err
+	}
+	return checksums, nil
+}
