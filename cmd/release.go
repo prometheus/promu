669844441ce0fcc2e74633a3936c98280048bc33
Simon Pasquier <spasquie@redhat.com>
2019-04-17 10:42:24 +0200

    Remove use of github-release
    Signed-off-by: Simon Pasquier <spasquie@redhat.com>

--- cmd/release.go
+++ cmd/release.go
@@ -15,60 +15,160 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
+	"github.com/google/go-github/github"
 	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
 
 	"github.com/prometheus/promu/util/retry"
-	"github.com/prometheus/promu/util/sh"
 )
 
 var (
 	releasecmd     = app.Command("release", "Upload all release files to the Github release")
 	allowedRetries = releasecmd.Flag("retry", "Number of retries to perform when upload fails").
 			Default("2").Int()
+	overwriteDraft = releasecmd.Flag("overwrite", "Overwrite existing files (only effective if the release is a draft)").
+			Default("false").Bool()
 	releaseLocation = releasecmd.Arg("location", "Location of files to release").Default(".").Strings()
 )
 
 func runRelease(location string) {
-	if err := filepath.Walk(location, releaseFile); err != nil {
-		fatal(errors.Wrap(err, "Failed to upload all files"))
+	token := os.Getenv("GITHUB_TOKEN")
+	if len(token) == 0 {
+		fatal(errors.New("GITHUB_TOKEN not defined"))
+	}
+
+	ctx := context.Background()
+	client := github.NewClient(
+		oauth2.NewClient(
+			ctx,
+			oauth2.StaticTokenSource(
+				&oauth2.Token{AccessToken: token},
+			),
+		),
+	)
+
+	// Find the GitHub release matching with the tag. We need to list all
+	// releases because it is the only way to get draft releases too.
+	tag := fmt.Sprintf("v%s", projInfo.Version)
+	var release *github.RepositoryRelease
+	opts := &github.ListOptions{}
+	for {
+		releases, resp, err := client.Repositories.ListReleases(ctx, projInfo.Owner, projInfo.Name, opts)
+		if err != nil {
+			fatal(errors.Wrap(err, "failed to list releases"))
+		}
+		for _, r := range releases {
+			if r.GetTagName() == tag {
+				release = r
+				break
+			}
+		}
+		if release != nil || resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
+	}
+	if release == nil {
+		fatal(errors.Errorf("failed to find a release with tag %s", tag))
+	}
+
+	if err := filepath.Walk(location, releaseFile(ctx, client, release)); err != nil {
+		// Remove incomplete assets.
+		// See https://developer.github.com/v3/repos/releases/#response-for-upstream-failure
+		opts = &github.ListOptions{}
+		for {
+			assets, resp, err := client.Repositories.ListReleaseAssets(ctx, projInfo.Owner, projInfo.Name, release.GetID(), opts)
+			if err != nil {
+				break
+			}
+			for _, asset := range assets {
+				if strings.EqualFold(asset.GetState(), "starter") {
+					_, _ = client.Repositories.DeleteReleaseAsset(ctx, projInfo.Owner, projInfo.Name, asset.GetID())
+				}
+			}
+			if resp.NextPage == 0 {
+				break
+			}
+			opts.Page = resp.NextPage
+		}
+		fatal(errors.Wrap(err, "failed to upload all files"))
 	}
 }
 
-func releaseFile(path string, f os.FileInfo, err error) error {
-	if err != nil {
-		return err
-	}
-	if f.IsDir() {
+func releaseFile(ctx context.Context, client *github.Client, release *github.RepositoryRelease) func(string, os.FileInfo, error) error {
+	return func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if fi.IsDir() {
+			return nil
+		}
+
+		// Check if the asset has already been uploaded and remove it if it is a draft release.
+		filename := filepath.Base(path)
+		opts := &github.ListOptions{}
+		for {
+			assets, resp, err := client.Repositories.ListReleaseAssets(ctx, projInfo.Owner, projInfo.Name, release.GetID(), opts)
+			if err != nil {
+				return errors.Wrap(err, "failed to list release assets")
+			}
+			var stop bool
+			for _, asset := range assets {
+				if asset.GetName() == filename {
+					var err error
+					stop = true
+					if *overwriteDraft && release.GetDraft() {
+						_, err = client.Repositories.DeleteReleaseAsset(ctx, projInfo.Owner, projInfo.Name, asset.GetID())
+						if err != nil {
+							err = errors.Wrapf(err, "failed to delete existing asset %q", filename)
+						}
+					} else {
+						err = errors.Errorf("%q already exists", filename)
+					}
+					if err != nil {
+						return err
+					}
+					break
+				}
+			}
+			if stop || resp.NextPage == 0 {
+				break
+			}
+			opts.Page = resp.NextPage
+		}
+
+		maxAttempts := *allowedRetries + 1
+		err = retry.Do(func(attempt int) (bool, error) {
+			again := attempt < maxAttempts
+
+			f, err := os.Open(path)
+			if err != nil {
+				return again, err
+			}
+
+			_, _, err = client.Repositories.UploadReleaseAsset(
+				ctx,
+				projInfo.Owner, projInfo.Name, release.GetID(),
+				&github.UploadOptions{Name: filename},
+				f)
+			if err != nil {
+				time.Sleep(2 * time.Second)
+			}
+
+			return again, err
+		})
+		if err != nil {
+			return errors.Wrapf(err, "failed to upload %q after %d attempts", filename, maxAttempts)
+		}
+		fmt.Println(" > uploaded", filename)
+
 		return nil
 	}
-
-	filename := filepath.Base(path)
-	maxAttempts := *allowedRetries + 1
-	err = retry.Do(func(attempt int) (bool, error) {
-		err = uploadFile(filename, path)
-		if err != nil {
-			time.Sleep(2 * time.Second)
-		}
-		return attempt < maxAttempts, err
-	})
-	if err != nil {
-		return errors.Wrapf(err, "failed to upload %q after %d attempts", filename, maxAttempts)
-	}
-	fmt.Println(" > uploaded", filename)
-
-	return nil
-}
-
-func uploadFile(filename string, path string) error {
-	return sh.RunCommand("github-release", "upload",
-		"--user", projInfo.Owner,
-		"--repo", projInfo.Name,
-		"--tag", fmt.Sprintf("v%s", projInfo.Version),
-		"--name", filename,
-		"--file", path)
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/prometheus/promu
 
 require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v0.9.2 // indirect
@@ -8,6 +10,7 @@ require (
 	github.com/prometheus/common v0.2.0
 	github.com/prometheus/procfs v0.0.0-20190209105433-f8d8b3f739bd // indirect
 	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.2.2
